e6c6b9f31bf14e969f3c2d87f57171ce7c2015d2
kkw855 <kkw855@gmail.com>
2021-03-09 17:42:08 +0900

    Exercise 3.8 foldRight 로 리스트 만들기

--- src/main/scala/data_structures/List.scala
+++ src/main/scala/data_structures/List.scala
@@ -1,6 +1,8 @@
 package com.endsoul.fp.scala
 package data_structures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
@@ -12,11 +14,46 @@ object List {
 
   def sum[A](ints: List[Int]): Int = ints match {
     case Nil        => 0
-    case Cons(h, t) => h + sum(ints)
+    case Cons(h, t) => h + sum(t)
   }
 
   def tail[A](as: List[A]): List[A] = as match {
     case Nil        => Nil
     case Cons(_, t) => t
   }
+
+  def setHead[A](as: List[A], a: A): List[A] = as match {
+    case Nil        => Nil
+    case Cons(_, t) => Cons(a, t)
+  }
+
+  @tailrec
+  def drop[A](as: List[A], n: Int): List[A] =
+    if (n == 0) as
+    else
+      as match {
+        case Nil        => Nil
+        case Cons(_, t) => drop(t, n - 1)
+      }
+
+  @tailrec
+  def dropWhile[A](as: List[A])(f: A => Boolean): List[A] = as match {
+    case Nil                => Nil
+    case Cons(h, t) if f(h) => dropWhile(t)(f)
+    case _                  => as
+  }
+
+  def init[A](as: List[A]): List[A] = as match {
+    case Nil                    => Nil
+    case Cons(_, t) if t == Nil => Nil
+    case Cons(h, t)             => Cons(h, init(t))
+  }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil        => z
+    case Cons(h, t) => f(h, foldRight(t, z)(f))
+  }
+
+  def product2[A](as: List[Double]): Double =
+    foldRight(as, 1.0)(_ * _)
 }
--- src/test/scala/data_structures/ListTest.scala
+++ src/test/scala/data_structures/ListTest.scala
@@ -1,18 +1,17 @@
 package com.endsoul.fp.scala
 package data_structures
 
-import data_structures.List.{sum, tail}
+import data_structures.List._
 
-import org.scalatest.matchers.must.Matchers.contain
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
-import scala.language.postfixOps
-
 class ListTest extends UnitSpec {
+  val list: List[Int] = List(1, 2, 3, 4, 5)
+
   // Exercise 3.1
   "리스트" should "패턴 매칭 한다" in {
     assertResult(3) {
-      List(1, 2, 3, 4, 5) match {
+      list match {
         case Cons(x, Cons(2, Cons(4, _)))          => x
         case Nil                                   => 42
         case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -23,16 +22,41 @@ class ListTest extends UnitSpec {
   }
 
   // Exercise 3.2
-  it should "리스트의 첫 요소를 제거한다" in {
-    assertResult(true) {
-      tail(List(1, 2, 3, 4, 5)) == List(2, 3, 4, 5)
-    }
+  it should "첫 요소를 제거한다" in {
+    tail(list) shouldBe List(2, 3, 4, 5)
   }
 
   // Exercise 3.3
-  it should "리스트의 첫 요소를 다른 값으로 대체한다" in {
-    assertResult(true) {
-      setHead(List(1, 2, 3, 4, 5)) == List(2, 3, 4, 5)
-    }
+  it should "첫 요소를 다른 값으로 대체한다" in {
+    setHead(list, 100) shouldBe List(100, 2, 3, 4, 5)
   }
+
+  // Exercise 3.4
+  it should "목록에서 처음 n개의 요소를 제거한다" in {
+    drop(list, 3) shouldBe List(4, 5)
+  }
+
+  // Exercise 3.5
+  it should "주어진 술어와 부합하는 List 의 앞 요소들을 제거한다" in {
+    dropWhile(list)(_ < 4) shouldBe List(4, 5)
+  }
+
+  // Exercise 3.6
+  it should "마지막 요소룰 제외한 모든 요소로 이루어진 List 를 돌려준다" in {
+    init(list) shouldBe List(1, 2, 3, 4)
+  }
+
+  it should "리스트의 모든 요소들을 곱한다" in {
+    product2(List(1.0, 2.0, 3.0, 4.0, 5.0)) shouldBe 120
+  }
+
+  // Exercise 3.8
+  it should "z를 Nil 로, f를 Cons 로 바꾸면 리스트를 얻는다" in {
+    foldRight(list, Nil: List[Int])(Cons(_, _)) shouldBe list
+  }
+
+  // Exercise 3.9
+//  it should "목록의 길이를 계산한다" in {
+//    length(List(1, 2, 3, 4, 5)) shouldBe 5
+//  }
 }
