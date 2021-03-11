b328d35335e05eb444586a0631209fedb4b47e15
kkw855 <kkw855@gmail.com>
2021-03-11 17:04:45 +0900

    Exercise 3.29 트리 maximum 함수 구현

--- src/main/scala/data_structures/Tree.scala
+++ src/main/scala/data_structures/Tree.scala
@@ -15,4 +15,31 @@ object Tree {
     case Leaf(v)      => v
     case Branch(l, r) => maximum(l).max(maximum(r))
   }
+
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_)      => 1
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v)      => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v)      => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def size2[A](t: Tree[A]): Int =
+    fold(t)(_ => 1)(1 + _ + _)
+
+  def maximum2(t: Tree[Int]): Int =
+    fold(t)(a => a)(_ max _)
+
+  def depth2[A](t: Tree[A]): Int =
+    fold(t)(a => 1)((d1, d2) => 1 + (d1 max d2))
+
+  def map2[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)): Tree[B])((a, b) => Branch(a, b))
 }
--- src/test/scala/data_structures/TreeTest.scala
+++ src/test/scala/data_structures/TreeTest.scala
@@ -13,7 +13,29 @@ class TreeTest extends UnitSpec {
     size(tree) shouldBe 7
   }
 
+  // Exercise 3.26
   it should "가장 큰 요소를 돌려주는 함수 maximum 을 구현한다" in {
     maximum(tree) shouldBe 4
+  }
+
+  // Exercise 3.27
+  it should "트리의 뿌리(root)에서 임의의 잎으로의 가장 긴 경로의 길이를 돌려주는 함수 depth 를 구현한다" in {
+    depth(tree) shouldBe 3
+  }
+
+  // Exercise 3.28
+  it should "List 에 대한 함수 map 과 비슷하게 트리의 각 요소를 주어진 함수로 수정하는 함수 map 을 구현한다" in {
+    map(tree)(_ * 10) shouldBe Branch(Branch(Leaf(10), Leaf(20)),
+                                      Branch(Leaf(30), Leaf(40)))
+  }
+
+  // Exercise 3.29
+  it should "size 와 maximum, depth, map 의 유사성을 요약해서 일반화한 새 함수 fold 를 구현하고 " +
+    "그 함수들을 새 fold 를 이용해서 다시 구현한다" in {
+    size2(tree) shouldBe 7
+    maximum2(tree) shouldBe 4
+    depth2(tree) shouldBe 3
+    map2(tree)(_ * 10) shouldBe Branch(Branch(Leaf(10), Leaf(20)),
+                                       Branch(Leaf(30), Leaf(40)))
   }
 }
