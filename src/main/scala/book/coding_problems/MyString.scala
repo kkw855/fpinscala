5c3c7cdb253ec0ae5dacdfd45752d7f0bed9e61f
endsoul <kkw855@gmail.com>
2021-05-21 17:56:17 +0900

    coding_problems no.7

--- src/main/scala/book/coding_problems/MyString.scala
+++ src/main/scala/book/coding_problems/MyString.scala
@@ -3,19 +3,57 @@ package book.coding_problems
 
 import scala.jdk.StreamConverters._
 import scala.util.Try
+import scala.collection.immutable.ListMap
 
+import cats._
+import cats.implicits._
+
+// TODO: 자바 throws API 처리 Option or Try or Either
 object MyString {
-  def countDuplicateCharacters(str: String): Map[String, Long] = {
-    val codePoints: LazyList[Int] = str.codePoints.toScala(LazyList)
-    codePoints
+  // cover ASCII, 16-bit Unicode, and Unicode surrogate pairs
+  def countDuplicateCharacters(str: String): Map[String, Long] =
+    // 하나의 문자를 4bytes(Int) 배열로 변환
+    str.codePoints
+      .toScala(LazyList)
       .map(c => Try(Character.toChars(c)).toOption)
-      .map(c => c.map(a => String.valueOf(a)))
-      .flatten
-      .foldLeft(Map.empty[String, Long])((m, str) => {
-        m.get(str) match {
-          case Some(v) => m + (str -> (v + 1))
-          case None    => m + (str -> 1)
+      .flatMap(_ map String.valueOf)
+      .foldLeft(ListMap.empty[String, Long])((acc, str) => {
+        acc.get(str) match {
+          case Some(v) => acc + (str -> (v + 1))
+          case None    => acc + (str -> 1)
+        }
+      })
+
+  def firstNonRepeatedCharacter(str: String): String =
+    countDuplicateCharacters(str)
+      .dropWhile(pair => pair._2 != 1)
+      .headOption
+      .getOrElse("", 0)
+      ._1
+
+  def reverseWords(str: String): String =
+    str
+      .split("""\s+""")
+      .to(LazyList)
+      .map(_.reverse)
+      .mkString(" ")
+
+  def containsOnlyDigits(str: String): Boolean =
+    str.matches("[0-9]+")
+
+  def countVowelsAndConsonants(str: String): (Long, Long) = {
+    val vowels = Set('a', 'e', 'i', 'o', 'u')
+    str
+      .foldLeft((0L, 0L))((acc, char) => {
+        val (vowel, consonant) = acc
+        if (vowels.contains(char)) {
+          (vowel + 1, consonant)
+        } else {
+          (vowel, consonant + 1)
         }
       })
   }
+
+  def countOccurrencesOfACertainCharacter(str: String, char: Char): Int =
+    str.count(ch => ch === char)
 }
--- src/test/scala/book/coding_problems/StringTest.scala
+++ src/test/scala/book/coding_problems/StringTest.scala
@@ -3,9 +3,11 @@ package book.coding_problems
 
 import MyString._
 
+import cats.implicits._
+
 //noinspection SpellCheckingInspection
 class StringTest extends UnitSpec {
-  "리스트" should "패턴 매칭 한다" in {
+  "StringTest" should "manipulate the string" in {
     // UTF-8 인코딩
     // 0000 0000 0010 1001
     "A".length shouldBe 1
@@ -42,11 +44,47 @@ class StringTest extends UnitSpec {
     codePoints(1) shouldBe 0x10400
   }
 
-  it should "패턴 매칭 한다!!!" in {
-    countDuplicateCharacters("aa🂡b🂡𐐀c𐐀") shouldBe Map(("a" -> 2),
-                                                          ("🂡" -> 2),
-                                                          ("b" -> 1),
-                                                          ("𐐀" -> 2),
-                                                          ("c" -> 1))
+  // 1. Counting duplicate characters
+  it should "count duplicate characters" in {
+    countDuplicateCharacters("aa🂡b🂡𐐀c𐐀") shouldBe Map("a" -> 2,
+                                                          "🂡" -> 2,
+                                                          "b" -> 1,
+                                                          "𐐀" -> 2,
+                                                          "c" -> 1)
+    countDuplicateCharacters("") shouldBe Map.empty
+  }
+
+  // 2. Finding the first non-repeated character
+  it should "find the first non-repeated character" in {
+    firstNonRepeatedCharacter("a🂡b𐐀ccba𐐀123") shouldBe "🂡"
+    firstNonRepeatedCharacter("") shouldBe ""
+  }
+
+  // 3. Reversing letters and words
+  it should "reverse letters and words" in {
+    reverseWords("1234 56 7     8") shouldBe "4321 65 7 8"
+  }
+
+  // 4. Checking whether a string contains only digits
+  it should "checking whether a string contains only digits" in {
+    containsOnlyDigits("012346789") shouldBe true
+    containsOnlyDigits("1 2") shouldBe false
+    containsOnlyDigits("+1") shouldBe false
+  }
+
+  // 5. Counting vowels and consonants
+  it should "count vowels and consonants" in {
+    countVowelsAndConsonants("abcdefghijklmn") shouldBe (3, 11)
+  }
+
+  // 6. Counting the occurrences of a certain character
+  it should "count the occurrences of a certain character" in {
+    countOccurrencesOfACertainCharacter("aa5555bb", '5') shouldBe 4
+  }
+
+  // 7. Converting a string into an int, long, float, or double
+  it should "converting a string into an int, long, float, or double" in {
+    Either.catchOnly[NumberFormatException](Integer.valueOf("123")) shouldBe 123
+      .asRight[NumberFormatException]
   }
 }
